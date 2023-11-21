0e86f8be7420c4dfaa1e75d45675c470ab1b02e9
Y wingpilot2 <136618828+Ywingpilot2@users.noreply.github.com>
2023-11-20 16:52:52 -0900

    Forgot some things

--- Models/Types/NodeTypes/Entity/Shared/ObjectReference/CharacterSpawnReferenceObjectData.cs
+++ Models/Types/NodeTypes/Entity/Shared/ObjectReference/CharacterSpawnReferenceObjectData.cs
@@ -121,6 +121,115 @@ namespace BlueprintEditorPlugin.Models.Types.NodeTypes.Entity.Shared.ObjectRefer
             }
         }
 
-        public override void OnModified(ItemModifiedEventArgs args) => OnCreation();
+        public override void OnModified(ItemModifiedEventArgs args)
+        {
+            switch (args.Item.Name)
+            {
+                case "Blueprint":
+                {
+                    PointerRef ptr = Object.Blueprint;
+
+                    EbxAssetEntry blueprintAssetEntry = App.AssetManager.GetEbxEntry(ptr.External.FileGuid);
+                    EbxAsset blueprint = App.AssetManager.GetEbx(blueprintAssetEntry);
+
+                    Name = $"LogicPrefab ({blueprintAssetEntry.Filename})";
+
+                    PointerRef interfaceRef = ((dynamic)blueprint.RootObject).Interface;
+                           
+                    //Populate interface outpts/inputs
+                    foreach (dynamic field in ((dynamic)interfaceRef.Internal).Fields)
+                    {
+                        if (field.AccessType.ToString() == "FieldAccessType_Source") //Source
+                        {
+                            if (GetOutput(field.Name, ConnectionType.Property) != null) continue;
+                            Outputs.Add(new OutputViewModel()
+                            {
+                                Title = field.Name,
+                                Type = ConnectionType.Property
+                            });
+                        }
+                        else if (field.AccessType.ToString() == "FieldAccessType_Target") //Target
+                        {
+                            if (GetInput(field.Name, ConnectionType.Property) != null) continue;
+                            Inputs.Add(new InputViewModel()
+                            {
+                                Title = field.Name,
+                                Type = ConnectionType.Property
+                            });
+                        }
+                        else //Source and Target
+                        {
+                            if (GetInput(field.Name, ConnectionType.Property) != null) continue;
+                            Inputs.Add(new InputViewModel()
+                            {
+                                Title = field.Name,
+                                Type = ConnectionType.Property
+                            });
+                    
+                            if (GetOutput(field.Name, ConnectionType.Property) != null) continue;
+                            Outputs.Add(new OutputViewModel()
+                            {
+                                Title = field.Name,
+                                Type = ConnectionType.Property
+                            });
+                        }
+                    }
+
+                    foreach (dynamic inputEvent in ((dynamic)interfaceRef.Internal).InputEvents)
+                    {
+                        if (GetInput(inputEvent.Name, ConnectionType.Event) != null) continue;
+                        Inputs.Add(new InputViewModel()
+                        {
+                            Title = inputEvent.Name,
+                            Type = ConnectionType.Event
+                        });
+                    }
+            
+                    foreach (dynamic outputEvent in ((dynamic)interfaceRef.Internal).OutputEvents)
+                    {
+                        if (GetOutput(outputEvent.Name, ConnectionType.Event) != null) continue;
+                        Outputs.Add(new OutputViewModel()
+                        {
+                            Title = outputEvent.Name,
+                            Type = ConnectionType.Event
+                        });
+                    }
+                
+                    foreach (dynamic inputLink in ((dynamic)interfaceRef.Internal).InputLinks)
+                    {
+                        if (GetInput(inputLink.Name, ConnectionType.Link) != null) continue;
+                        Inputs.Add(new InputViewModel()
+                        {
+                            Title = inputLink.Name,
+                            Type = ConnectionType.Link
+                        });
+                    }
+
+                    foreach (dynamic outputLink in ((dynamic)interfaceRef.Internal).OutputLinks)
+                    {
+                        if (GetInput(outputLink.Name, ConnectionType.Link) != null) continue;
+                        Outputs.Add(new OutputViewModel()
+                        {
+                            Title = outputLink.Name,
+                            Type = ConnectionType.Link
+                        });
+                    }
+                } break;
+                case "Template":
+                {
+                    PointerRef ptr = Object.Blueprint;
+                    if (ptr.External.FileGuid == System.Guid.Empty) return;
+            
+                    EbxAssetEntry blueprintAssetEntry = App.AssetManager.GetEbxEntry(ptr.External.FileGuid);
+                    EbxAsset blueprint = App.AssetManager.GetEbx(blueprintAssetEntry);
+                    
+                    PointerRef templatePointer = Object.Template;
+
+                    if (templatePointer.External.FileGuid == System.Guid.Empty) return;
+                    EbxAssetEntry templateAssetEntry = App.AssetManager.GetEbxEntry(ptr.External.FileGuid);
+                    Name = $"Character ({blueprintAssetEntry.Filename}, {templateAssetEntry.Filename})";
+                } break;
+            }
+        }
     }
 }
