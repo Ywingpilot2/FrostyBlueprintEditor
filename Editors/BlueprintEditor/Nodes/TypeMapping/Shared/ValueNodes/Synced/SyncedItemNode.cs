c424efa9c5212089572e7928f044dbd0cc268024
Nuuby <67482994+Nuuby29@users.noreply.github.com>
2024-06-22 16:42:49 -0700

    Update SyncedItemNode.cs

--- Editors/BlueprintEditor/Nodes/TypeMapping/Shared/ValueNodes/Synced/SyncedItemNode.cs
+++ Editors/BlueprintEditor/Nodes/TypeMapping/Shared/ValueNodes/Synced/SyncedItemNode.cs
@@ -1,17 +1,17 @@
-﻿using BlueprintEditorPlugin.Editors.BlueprintEditor.Connections;
+er﻿using BlueprintEditorPlugin.Editors.BlueprintEditor.Connections;
 using BlueprintEditorPlugin.Models.Entities.Networking;
 
 namespace BlueprintEditorPlugin.Editors.BlueprintEditor.Nodes.TypeMapping.Shared.ValueNodes.Synced
 {
     public abstract class SyncedNode : EntityNode
     {
-        public override string ToolTip => "This node syncs a value between client and server.";
+        public override string ToolTip => "This node syncs a value between all clients connected to the server.";
 
         public override void OnCreation()
         {
             base.OnCreation();
 
-            AddInput("In", ConnectionType.Property, Realm.Client);
+            AddInput("In", ConnectionType.Property, Realm.Server);
             AddOutput("Out", ConnectionType.Property, Realm.Server);
         }
     }
